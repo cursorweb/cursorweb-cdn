c13b9e014b4d642f4e946c474afddd4c816ffb98
Junhao "Jerry" Zhang <junhaodev@gmail.com>
2019-12-30 10:40:49 -0600

    Update bestbutton.js

--- bestbuttonv1/bestbutton.js
+++ bestbuttonv1/bestbutton.js
@@ -2,30 +2,34 @@
  * Created by coder100 on repl.it
  * NOTE: You need jquery
  */
-var bestButton = {
-  styles: {},
-  init: function(){
-    for(i in bestButton.styles){
-      var style = bestButton.styles[i];
-      $('.btn-'+i)
-      .css({
-          color: style[0].color || "#fff",
-          background: style[0].background
-        })
-      .hover(
-        function(){
-          $(this).css({
-            color: style[1].color || style[0].color || "#fff",
-            background: style[1].background
-          })
-        },
-        function(){
-          $(this).css({
+if(window.$){
+  var bestButton = {
+    styles: {},
+    init: function(){
+      for(i in bestButton.styles){
+        var style = bestButton.styles[i];
+        $('.btn-'+i)
+        .css({
             color: style[0].color || "#fff",
             background: style[0].background
           })
-        }
-      )
+        .hover(
+          function(){
+            $(this).css({
+              color: style[1].color || style[0].color || "#fff",
+              background: style[1].background
+            })
+          },
+          function(){
+            $(this).css({
+              color: style[0].color || "#fff",
+              background: style[0].background
+            })
+          }
+        )
+      }
     }
-  }
-};
+  };
+}else{
+  console.log("Please install jQuery for bestbutton to work.");
+}
